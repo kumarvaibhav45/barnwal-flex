df27d66f64e26e5f25b3a153bf62f606bc1ccc31
kumarvaibhav45 <wave.vaibhav@gmail.com>
2020-12-24 00:05:00 +0530

    minor ui fixes

--- components/popup-model.js
+++ components/popup-model.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const PopupModel = ({ img, title, close, width, height }) => (
-  <div className='flex flex-col'>
+  <div className='flex flex-col h-full'>
     <Image
       src={img}
       width={width}
@@ -12,10 +12,10 @@ const PopupModel = ({ img, title, close, width, height }) => (
       unoptimized={true}
     />
     <div className='flex justify-between mt-2 text-xs md:text-sm'>
-      <p>{title}</p>
+      <p className='text-sm md:text-base'>{title}</p>
       <button
         onClick={close}
-        className='text-2xl w-5 h-5 flex justify-center items-center rounded-none border border-black hover:bg-black  hover:text-white transition-all duration-100 lg:w-6 lg:h-6 lg:text-3xl'
+        className='text-xl w-5 h-5 flex justify-center items-center rounded-none border border-black hover:bg-black  hover:text-white transition-all duration-100 lg:w-6 lg:h-6'
       >
         &times;
       </button>
