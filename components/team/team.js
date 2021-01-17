85cd0b00b387a753a2a034b3413989848c25e3b0
kumarvaibhav45 <wave.vaibhav@gmail.com>
2021-01-17 23:05:43 +0530

    minor ui fix & reduce team

--- components/team/team.js
+++ components/team/team.js
@@ -7,16 +7,20 @@ export default function Team({ members }) {
       <div className='centered-container'>
         <SectionTitle name="Director's diary" />
         <div className='grid grid-cols-1 gap-y-12 md:grid-cols-3 md:gap-x-10 mt-16 lg:gap-x-14'>
-          {members?.map(({ id, name, designation, img, message }) => (
-            <MemberCard
-              key={id}
-              name={name}
-              designation={designation}
-              img={img}
-              message={message}
-              id={id}
-            />
-          ))}
+          {members?.map(({ id, name, designation, img, message }) =>
+            id !== '1' ? (
+              <MemberCard
+                key={id}
+                name={name}
+                designation={designation}
+                img={img}
+                message={message}
+                id={id}
+              />
+            ) : (
+              ''
+            )
+          )}
         </div>
       </div>
     </div>
